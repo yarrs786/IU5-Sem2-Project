75e64a38f8ce3678de8e960205018fbe1c628ceb
NewDawnPRG <vremenno44@gmail.com>
2025-02-23 02:17:34 +0300

    Making Struct
    First step of making project struct. Added all the libraries we are going to create.

---
+++ game/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+public:
+    // Code
+private:
+    // Code
+};
+
+#endif //GAME_H
---
+++ piece/piece.h
@@ -0,0 +1,11 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+class Piece {
+public:
+    // Code
+private:
+    // Code
+};
+
+#endif //PIECE_H
